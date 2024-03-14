b851f9212437777a0e41c256a1345bca89f67161
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-14 20:41:55 +0100

    Mise à jour des dépendances et des styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image()]
+  site: "https://eloick.fr",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image(),
+    icon({
+      include: {
+        mdi: ["*"],
+        logos: ["*"],
+      },
+    }),
+  ],
 });
