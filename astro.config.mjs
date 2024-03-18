d857a056478603897c938630f44735344861d762
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-18 14:31:10 +0100

    Ajouter les intégrations Vercel et l'adaptateur Vercel, mettre à jour les icônes et les liens externes, et ajouter des informations sur les projets.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,17 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://eloick.fr",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    image(),
-    icon({
-      include: {
-        mdi: ["*"],
-        logos: ["*"],
-      },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), image(), icon({
+    include: {
+      mdi: ["*"],
+      logos: ["*"]
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
